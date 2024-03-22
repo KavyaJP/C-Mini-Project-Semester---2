63c0108d91bc2979af3fbe0c520d0a75cb5d32b8
Kavya Prajapati <kavya31052005@gmail.com>
2024-03-22 07:38:14 +0530

    Alpha 2.0 TicTacToe

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,27 +3,67 @@ using namespace std;
 void printgrid(char[3][3]);
 int main()
 {
+    int choice1, choice2;
     char a[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-    printgrid(a);
-    int choice;
-    cout << "(1) Human Vs Human" << endl
-         << "(2) Human Vs Computer" << endl;
-label_choice:
-    cin >> choice;
-    switch (choice)
+    cout << "(1) Start The Game" << endl
+         << "(2) Basic Rules" << endl
+         << "(3) Exit The Game" << endl;
+label_choice1:
+    cin >> choice1;
+    switch (choice1)
     {
     case 1:
+        cout << "Welcome to Tic Tac Toe" << endl;
+        cout << "(1) Human Vs Human" << endl
+             << "(2) Human Vs Computer" << endl;
+    label_choice2:
+        cin >> choice2;
+        switch (choice2)
+        {
+        case 1:
+            cout << endl
+                 << endl
+                 << "----------------" << endl
+                 << "|              |" << endl
+                 << "|Human Vs Human|" << endl
+                 << "|              |" << endl
+                 << "----------------" << endl
+                 << endl;
+            printgrid(a);
+            break;
+        case 2:
+            cout << endl
+                 << endl
+                 << "----------------" << endl
+                 << "|              |" << endl
+                 << "|Human Vs Human|" << endl
+                 << "|              |" << endl
+                 << "----------------" << endl
+                 << endl;
+
+            break;
+        default:
+            cout << "Invalid choice, Enter Your Choice Again : " << endl;
+            goto label_choice2;
+            break;
+        }
         break;
     case 2:
+        cout << endl
+             << endl
+             << "Tic Tac Toe Rules" << endl;
+        break;
+    case 3:
         break;
     default:
         cout << "Invalid choice, Enter Your Choice Again : " << endl;
-        goto label_choice;
+        goto label_choice1;
         break;
     }
 }
 void printgrid(char a[3][3])
 {
+    cout << "------------------" << endl;
     for (int i = 0; i < 3; i++)
     {
         cout << "  ";
