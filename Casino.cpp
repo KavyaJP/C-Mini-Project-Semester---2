bc73cb56c220a491de4ba9a0bb914bc94a9fddda
KavyaJP <145911611+KavyaJP@users.noreply.github.com>
2024-03-28 20:16:01 +0530

    Just Formatting

--- Casino.cpp
+++ Casino.cpp
@@ -1,50 +1,52 @@
-#include<iostream>
-#include<cstdlib>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
-class Game {
+class Game
+{
 public:
     int num1;
     int n;
-
-    Game() {
-        num1 = 1+(rand()%100);
-//        System.out.println(num1);
+    Game()
+    {
+        num1 = 1 + (rand() % 100);
     }
-    void takeUserInput() {
-        cout<<"Guess the number"<<endl;
-        cin>> n;
+    void takeUserInput()
+    {
+        cout << "Guess the number" << endl;
+        cin >> n;
     }
-    bool isCorrect(){
-        if(n == num1){
+    bool isCorrect()
+    {
+        if (n == num1)
+        {
             return true;
         }
-        else{
-                return false;
+        else
+        {
+            return false;
         }
     }
 };
-    int main()
+int main()
+{
+    bool b = false;
+    Game g;
+    int count = 0;
+    while (b != true)
     {
-        bool b=false;
-        Game g;
-        int count=0;
-       while (b!=true )
-       {
-           g.takeUserInput();
-           if(g.n< g.num1)
-           {
-               cout<<"Too small try again"<<endl;
-           }
-           if(g.n> g.num1)
-           {
-               cout<<"Too large try again"<<endl;
-           }
-           b= g.isCorrect();
-           count++;
-
-       }
-        cout<<"\n\n Congrats You won!!\n No. of guesses are:"<< count<<endl;
+        g.takeUserInput();
+        if (g.n < g.num1)
+        {
+            cout << "Too small try again" << endl;
+        }
+        if (g.n > g.num1)
+        {
+            cout << "Too large try again" << endl;
+        }
+        b = g.isCorrect();
+        count++;
+    }
+    cout << "\n\n Congrats You won!!\n No. of guesses are:" << count << endl;
     return 0;
 }
-
