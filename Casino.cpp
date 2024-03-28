d0b2b329216f197e7b60e606e8583eceee53b620
Puja-Rachchh <143953428+Puja-Rachchh@users.noreply.github.com>
2024-03-28 19:44:29 +0530

    Casino game- Guess the number

---
+++ Casino.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+class Game {
+public:
+    int num1;
+    int n;
+
+    Game() {
+        num1 = 1+(rand()%100);
+//        System.out.println(num1);
+    }
+    void takeUserInput() {
+        cout<<"Guess the number"<<endl;
+        cin>> n;
+    }
+    bool isCorrect(){
+        if(n == num1){
+            return true;
+        }
+        else{
+                return false;
+        }
+    }
+};
+    int main()
+    {
+        bool b=false;
+        Game g;
+        int count=0;
+       while (b!=true )
+       {
+           g.takeUserInput();
+           if(g.n< g.num1)
+           {
+               cout<<"Too small try again"<<endl;
+           }
+           if(g.n> g.num1)
+           {
+               cout<<"Too large try again"<<endl;
+           }
+           b= g.isCorrect();
+           count++;
+
+       }
+        cout<<"\n\n Congrats You won!!\n No. of guesses are:"<< count<<endl;
+    return 0;
+}
+
